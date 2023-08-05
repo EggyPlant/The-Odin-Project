7e250ceca2372a64f5da411f9a3a1e2d7ce9772f
EggyPlant <mroutrageous99@gmail.com>
2023-08-05 16:30:54 -0700

    variable exercises

---
+++ ruby01/name.rb
@@ -0,0 +1,35 @@
+puts 'Hello! Please input your name!'
+
+name = gets.chomp
+
+puts "Nice to meet you " + name + "!"
+
+# *******
+10.times {  puts name }
+
+# *******
+puts 'Please enter your first name'
+
+first_name = gets.chomp
+
+puts 'Please enter your last name'
+
+last_name = gets.chomp
+
+puts "your full name is " + first_name + " " + last_name
+
+
+# ********
+x = 0
+3.times do
+    x += 1
+end
+puts x
+
+# *******
+# y = 0
+# 3.times do
+#     y += 1
+#     x = y
+# end
+# puts x
