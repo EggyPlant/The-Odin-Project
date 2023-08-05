12db6f2cc17f34542dad5a519cdde12da205e6eb
EggyPlant <mroutrageous99@gmail.com>
2023-08-04 18:32:34 -0700

    added additional file

---
+++ ruby01/source.rb
@@ -0,0 +1,28 @@
+a = "Mikey"
+b = "Chen"
+
+puts "#{a} " + "#{b}"
+
+# puts 1234 / 1
+# puts 1234 / 10
+# puts 1234 / 100
+# puts 1234 / 1000
+
+movies = {:Drive => 2011, :ThereWillBeBlood => 2007, :PhantomThread => 2018}
+
+puts movies[:Drive]
+puts movies[:ThereWillBeBlood]
+puts movies[:PhantomThread]
+
+a = [movies[:Drive], movies[:ThereWillBeBlood], movies[:PhantomThread]]
+puts a[0]
+puts a[1]
+puts a[2]
+
+num1 = 10.34
+num2 = 42.25
+num3 = 100.00
+
+puts num1 * num1
+puts num2 * num2
+puts num3 * num3
